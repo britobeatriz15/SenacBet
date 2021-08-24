0137a43e618c82e7528001c22cf641b4b865cfb7
britobeatriz15 <britobeatriz15@gmail.com>
2021-08-24 20:13:10 -0300

    Add CadastroApostador e ApostadorDAO.

--- app/src/main/java/com/example/senacbet/CadastroApostador.java
+++ app/src/main/java/com/example/senacbet/CadastroApostador.java
@@ -3,6 +3,11 @@ package com.example.senacbet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.senacbet.DAO.ApostadorDAO;
 
 public class CadastroApostador extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class CadastroApostador extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_apostador);
+
+        Button buttonSalvarApostador = findViewById(R.id.activity_cadastro_button_cadastrar_apostador);
+        buttonSalvarApostador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ApostadorDAO apostadorDAO = new ApostadorDAO();
+
+                EditText nomeApostador = findViewById(R.id.activity_cadastro_nome_do_apostador);
+                apostadorDAO.setNomeApostador(nomeApostador.getText().toString());
+
+                EditText saldo = findViewById(R.id.activity_cadastro_saldo);
+                apostadorDAO.setSaldo(saldo.getText().toString());
+
+                EditText email = findViewById(R.id.activity_cadastro_email);
+                apostadorDAO.setEmail(email.getText().toString());
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/senacbet/DAO/ApostadorDAO.java
@@ -0,0 +1,31 @@
+package com.example.senacbet.DAO;
+
+public class ApostadorDAO {
+    String nomeApostador;
+    String saldo;
+    String email;
+
+    public String getNomeApostador() {
+        return nomeApostador;
+    }
+
+    public void setNomeApostador(String nomeApostador) {
+        this.nomeApostador = nomeApostador;
+    }
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
